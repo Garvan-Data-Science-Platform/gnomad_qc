1e5b7aaa6f96d3dbae7449fbc885b8e16950fca6
Matthew Hobbs <m.hobbs@garvan.org.au>
2024-08-16 09:32:41 +1000

    update annotation source import

--- gnomad_qc/v2/load_data/import_resources.py
+++ gnomad_qc/v2/load_data/import_resources.py
@@ -57,8 +57,8 @@ def import_methylation(overwrite: bool = False):
 
 
 def import_exac_data(overwrite: bool = False):
-    vcf_path = "gs://gnomad/raw/source/ExAC.r1.sites.vep.vcf.gz"
-    vds = hl.import_vcf(vcf_path, force_bgz=True, min_partitions=5000).rows()
+    vcf_path = "gs://gcp-public-data--gnomad/legacy/exac_browser/ExAC.r1.sites.vep.vcf.gz"
+    vds = hl.import_vcf(vcf_path, force_bgz=True, min_partitions=5000, array_elements_required=False).rows()
     vds = hl.split_multi_hts(vds)
     vds = hl.vep(vds, vep_config)
     vds.write(exac_release_sites_ht_path(), overwrite)
